50acfcb9c23c051f038402aa1466e86792eac463
daniel conroy <dcconroy3@gmail.com>
2021-04-06 00:18:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,6 @@
 function add() {
   return a += b
 }
-
-/*describe('basic math functions', function () {
-  it("'add()' is a valid function", function() {
-    expect(add).toExist
-  })*/
   
 function subtract() {
 

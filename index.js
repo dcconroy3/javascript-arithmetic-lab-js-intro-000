032982a592ddaf48c2725c5f0dc581c0ea5b3242
daniel conroy <dcconroy3@gmail.com>
2021-04-06 01:15:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function add(a,b) {
-  return a + b
+  return a + b;
 }
 
-function subtract() {
-
+function subtract(a,b) {
+  return a - b
 }
 
-function multiply() {
-
+function multiply(a,b) {
+  return a * b
 }
 
-function divide() {
-
+function divide(a,b) {
+  return a / b
 }
 
 function increment(n) {
